c9b79e260ad00665a55216b72ea886d96bb33a8a
Mat <matiaibarr43@gmail.com>
2026-08-13 08:48:56 -0300

    Eliminando Dockerfile y output: standalone de NextConfig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['d67a-200-122-69-93.ngrok-free.app'],
-  output: "standalone",
 };
 
 export default nextConfig;
